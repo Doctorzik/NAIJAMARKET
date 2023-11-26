6515ad58f2e09995246f002b2d4588b5c376fba2
doctorzik <ezekieljeremiah92@gmail.com>
2023-11-26 21:21:39 +0100

    jjj

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
-import { addSum } from "./mjs/utilities.mjs";
+import { doc } from "prettier";
+import { addSum , apiFetch, setLocalStorage, getlocalStorage, qs } from "./mjs/utilities.mjs";
 
 
+const url = "https://dummyjson.com/quotes?skip&limit=100"
+
+fetchQoutes()
+
+async function  fetchQoutes(){
+    const  data =  await apiFetch(url)
+
+ console.log(data.quotes)
+ const randomIndex = Math.floor(Math.random() * data.quotes.length );
+
+ const randomQoute =  data.quotes[randomIndex].quote
+ const author =  data.quotes[randomIndex].author
+ console.log(randomQoute )
+ console.log(author )
+    const me = document.createElement("div")
+     const      quotele = document.createComment("quote").innerHtml =  randomQoute
+             const p  = document.createComment("p").innerHtml = author
+    qs("#quote-div").append(quotele, p)
+
+}
